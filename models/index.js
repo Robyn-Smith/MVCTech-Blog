125282c29cb770da703e3f9d586db226cd85a068
Robyn Smith <robynsmith21@hotmail.com>
2023-07-29 22:52:36 +0100

    starter css project 2

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
-const Comment = require('./Comment');
+const Comment = require('.Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
