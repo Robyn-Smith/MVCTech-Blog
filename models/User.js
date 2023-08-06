f514f2f6d5ffd7c6cc09b81517b6e7790c817b31
Robyn Smith <robynsmith21@hotmail.com>
2023-08-06 19:12:49 +0100

    changes to user.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+  }}
 
 User.init(
   {
@@ -14,36 +13,36 @@ User.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
-      },
-    },
+        len: [8]
+      }
+    }
   },
   {
     hooks: {
       beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        newUserData.password = await bcrypt.hash(newUserData.password, 15);
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 15);
         return updatedUserData;
-      },
+      }
     },
     sequelize,
+    modelName: 'user',
     timestamps: false,
     freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
+    underscored: true
   }
 );
 
