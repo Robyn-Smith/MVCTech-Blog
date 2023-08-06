5281a496f8574debe6fcaf51e34e7274976767ed
Robyn Smith <robynsmith21@hotmail.com>
2023-08-06 19:15:50 +0100

    changes to comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,26 +1,28 @@
 const HandleComment = async (event) => {
-    event.preventDefault();
-  
-    const comment = document.querySelector('#comment').value.trim();
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
-    
-    if (comment) {
-      const result = await fetch(`/api/blog/${id}`, {
-        method: 'POST',
-        body: JSON.stringify({comment}),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (result.ok) {
-        document.location.replace(`/blog/${id}`);
-      } else {
-        alert('Unable to add comment.');
+  event.preventDefault();
+
+  const comment = document.querySelector('#comment').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  if (comment) {
+    const result = await fetch(`/api/blog/${id}`, {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+
+    if (result.ok) {
+      document.location.replace(`/blog/${id}`);
+    } else {
+      alert('Unable to add comment.');
     }
-  };
-  
-  document
-    .querySelector('.comment-form')
-    .addEventListener('submit', HandleComment);
+  }
+};
+
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', HandleComment);
