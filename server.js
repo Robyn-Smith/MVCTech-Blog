53a047ca6f39be5838062750a7b77a8c9cab866a
Robyn Smith <robynsmith21@hotmail.com>
2023-08-06 18:21:24 +0100

    changed technology-blogging tech-blog + comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Import required modules
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -8,34 +9,40 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
+// Create an Express application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Configure session expires in one hour
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 60 * 60 * 1000,   
-  },// expires in one hour
+    maxAge: 60 * 60 * 1000, 
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
 };
-
 app.use(session(sess));
 
+// Configure Handlebars engine
 const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware for JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use defined routes
 app.use(routes);
 
+// Sync Sequelize models and start server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Server is now listening on port', PORT));
 });
