b50881888289e26adc984748d4c1510b9b0f5d5d
Robyn Smith <robynsmith21@hotmail.com>
2023-08-06 18:28:54 +0100

    changes to layout of user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,23 +1,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// CREATE new user - handling signup
+// CREATE new user - signup
 router.post('/signup', async (req, res) => {
   try {
+    await console.log(req.body);
 
-    await console.log(req.body);  
     const userInfoDB = await User.create({
       username: req.body.username,
       password: req.body.password,
     });
-    console.log(userInfoDB)
+
+    console.log(userInfoDB);
+
     req.session.save(() => {
       req.session.loggedIn = true;
       req.session.username = req.body.username;
-
-      // redirect back to / after signup successfully
-      res.redirect('/');        
-
+      res.redirect('/');
     });
   } catch (err) {
     console.log(err);
@@ -35,28 +34,21 @@ router.post('/login', async (req, res) => {
     });
 
     if (!userInfoDB) {
-      res
-        .status(400)
-        .json({ message: 'Unable to login' });
+      res.status(400).json({ message: 'Unable to login' });
       return;
     }
 
     const validPassword = await userInfoDB.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Unable to login' });
+      res.status(400).json({ message: 'Unable to login' });
       return;
     }
 
     req.session.save(() => {
       req.session.loggedIn = true;
       req.session.username = req.body.username;
-
-      // redirect to / on successful login
-      res.redirect('/');        
-      
+      res.redirect('/');
     });
   } catch (err) {
     console.log(err);
@@ -64,7 +56,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// Logout 
+// Logout
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
